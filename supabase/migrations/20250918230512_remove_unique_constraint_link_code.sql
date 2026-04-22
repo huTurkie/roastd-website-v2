9dd0d4126ca58d5ada9fe928eeac5126a8e6b1ee
EL HASSAINE <hassaine@ELs-MacBook-Air.local>
2026-04-21 20:45:32 -0400

    Ignore non-web directories

--- supabase/migrations/20250918230512_remove_unique_constraint_link_code.sql
+++
@@ -1,2 +0,0 @@
--- Remove UNIQUE constraint from link_code to allow multiple users per link
-ALTER TABLE public.roast_sessions DROP CONSTRAINT IF EXISTS roast_sessions_link_code_key;
--- supabase/migrations/20250918231319_remove_not_null_constraint_link_code.sql
+++
@@ -1,2 +0,0 @@
--- Remove NOT NULL constraint from link_code to allow null values
-ALTER TABLE public.roast_sessions ALTER COLUMN link_code DROP NOT NULL;
